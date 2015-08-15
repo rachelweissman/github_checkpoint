9ce84214c26cc6b480fa0b833ef8e22d2ba5b8ed
rachelweissman <racheldweissman@gmail.com>
2015-08-15 16:43:11 -0500

    Update landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -3,12 +3,6 @@ var revealPoint = function(idx) {
   points[idx].style.transform = "scaleX(1) translateY(0)";
   points[idx].style.msTransform = "scaleX(1) translateY(0)";
   points[idx].style.WebkitTransform = "scaleX(1) translateY(0)";
-  points[idx].style.transition = "all 2s ease-in-out";
-  points[idx].style.msTransition = "all 2s ease-in-out";
-  points[idx].style.WebkitTransition = "all 2s ease-in-out";
-  points[idx].style.transitionDelay = "1s";
-  points[idx].style.msTransitionDelay = "1s";
-  points[idx].style.WebkitTransitionDelay = "1s";
 };
 
 var points = document.getElementsByClassName('point');
