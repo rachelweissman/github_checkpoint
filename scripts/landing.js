6b9e40b9bd90ee9a3a922911a65de42e4040abf2
Rachel <racheldweissman@gmail.com>
2015-08-15 16:41:12 -0500

    Assignment 22 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,22 @@
-var animatePoints = function() {
-
-    var points = document.getElementsByClassName('point');
-
-    var revealFirstPoint = function() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";   
-    };
-
-    var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";   
-    };
-
-    var revealThirdPoint = function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";   
-    };
-
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
-
+var revealPoint = function(idx) {  
+  points[idx].style.opacity = 1;
+  points[idx].style.transform = "scaleX(1) translateY(0)";
+  points[idx].style.msTransform = "scaleX(1) translateY(0)";
+  points[idx].style.WebkitTransform = "scaleX(1) translateY(0)";
+  points[idx].style.transition = "all 2s ease-in-out";
+  points[idx].style.msTransition = "all 2s ease-in-out";
+  points[idx].style.WebkitTransition = "all 2s ease-in-out";
+  points[idx].style.transitionDelay = "1s";
+  points[idx].style.msTransitionDelay = "1s";
+  points[idx].style.WebkitTransitionDelay = "1s";
 };
+
+var points = document.getElementsByClassName('point');
+
+for (var i = 0; i< points.length; i++) {
+    revealPoint(i);
+}
+
+
+
+
